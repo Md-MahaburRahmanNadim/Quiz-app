9424cbdc3755a0d3ae8acc24a4ada65f03310b21
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2025-01-17 15:08:31 +0600

    -m

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,34 +10,18 @@ export default function QuestionCard({ question }: QuestionCard) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{question.title}</Text>
-      <AnswerOption
-        option={question.options[0]}
-        isSelected={question.options[0] === SelectedOption}
-        onPress={() => {
-          Alert.alert(question.options[0], "option is pressed");
-        }}
-      />
-      <AnswerOption
-        option={question.options[1]}
-        isSelected={question.options[1] === SelectedOption}
-        onPress={() => {
-          Alert.alert(question.options[1], "option is pressed");
-        }}
-      />
-      <AnswerOption
-        option={question.options[2]}
-        isSelected={question.options[2] === SelectedOption}
-        onPress={() => {
-          Alert.alert(question.options[2], "option is pressed");
-        }}
-      />
-      <AnswerOption
-        option={question.options[3]}
-        isSelected={question.options[3] === SelectedOption}
-        onPress={() => {
-          Alert.alert(question.options[3], "option is pressed");
-        }}
-      />
+      {question.options.map((option) => {
+        return (
+          <AnswerOption
+            key={option}
+            option={option}
+            isSelected={option === SelectedOption}
+            onPress={() => {
+              Alert.alert(option, "option is pressed");
+            }}
+          />
+        );
+      })}
     </View>
   );
 }
